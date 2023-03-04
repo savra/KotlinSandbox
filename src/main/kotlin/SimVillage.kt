2083524ea92126aff3523764d09b576cc85b78ff
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-04 12:20:40 +0300

    Завершил главу 6

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,6 +1,21 @@
 fun main(args: Array<String>) {
-    println({
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+    println(greetingFunction("Guyal"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+
+    return { playerName: String ->
         val currentYear = 2018
-        "Welcome to SimVillage, Mayor! (copyright $currentYear)"
-    }())
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
+    }
 }
