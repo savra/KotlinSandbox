da5552b2a293611a9ddd79b71dc66ff511153bd2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-20 11:16:51 +0300

    Расширения

---
+++ src/main/kotlin/Variance.kt
@@ -0,0 +1,7 @@
+class Barrel<in T>(item: T)
+
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+    fedoraBarrel = lootBarrel 
+}
